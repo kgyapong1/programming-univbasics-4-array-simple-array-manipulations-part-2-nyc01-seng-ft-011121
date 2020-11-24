3cdcdda4b78510c95d9b1b653b80320f4bfe6da9
Kwaku Gyapong <kwakugyapong219@gmail.com>
2020-11-24 18:02:34 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++
@@ -1,5 +0,0 @@
-describe "#using_concat" do 
-  before(:each) do 
-    @my_favorite_things = ["raindrops on roses", "whiskers on kittens"]
-    more_favs = ["mario kart", "flatiron school"]
-    @all_my_favs = using_concat(@my_favorite_things, more_favs)
